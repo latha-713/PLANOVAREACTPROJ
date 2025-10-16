b872727d3279694a873d3715013f160386ae967d
latha-713 <lathanagasamudram729@gmail.com>
2025-10-16 11:02:20 +0530

    16th oct Taskform completely

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
+
 import Taskform from "./Components/Taskform"
 import Tasklist from "./Components/TaskList"
 import Progresstracker from "./Components/Progresstracker"
+import { useEffect, useState } from "react"
 
 export default function App() {
+      const[tasks, setTasks] = useState([]);
+
+        useEffect(()=> {
+          localStorage.setItem
+          ("tasks", JSON.stringify(tasks))
+        });
+
+      const addTask = (task) => {
+         setTasks([...tasks,task]);
+      }
+
   return(
     <div>
       <h1>PLANOVA</h1>
       <p>Our friendly TaskManager</p>
-      <Taskform />
+      <Taskform addTask = {addTask}/>
       <Tasklist />
       <Progresstracker />
       <button>Clear all tasks</button>
     </div>
   )
 }
-
  
