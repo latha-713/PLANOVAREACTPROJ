06d34e0cc6b652fb4497317d3154f17fc5fb20ce
latha-713 <lathanagasamudram729@gmail.com>
2025-10-17 11:32:20 +0530

    17th oct TaskList pt

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,44 @@
-
-import Taskform from "./Components/Taskform"
-import Tasklist from "./Components/TaskList"
-import Progresstracker from "./Components/Progresstracker"
-import { useEffect, useState } from "react"
+import Taskform from "./Components/Taskform";
+import TaskList from "./Components/TaskList";
+import Progresstracker from "./Components/Progresstracker";
+import { useEffect, useState } from "react";
+import "./Style.css"
 
 export default function App() {
-      const[tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
-        useEffect(()=> {
-          localStorage.setItem
-          ("tasks", JSON.stringify(tasks))
-        });
+  useEffect(() => {
+    localStorage.setItem
+    ("tasks", JSON.stringify(tasks))
+  });
 
-      const addTask = (task) => {
-         setTasks([...tasks,task]);
-      }
+  const addTask = (task) => {
+    setTasks([...tasks,task]);
+  }
+
+  const updateTask = (updatedTask, index) => {
+    const newtask = [...tasks];
+     newtask[index] = updatedTask;
+    setTasks(newtask);
+  }
+
+  const deleteTask = (index) => {
+      setTasks(tasks.filter((_, i) => i != index));
+  }
 
   return(
-
+    
     <div>
-      <h1>PLANOVA</h1>
+      <h1>Task Focus</h1>
       <p>Our friendly TaskManager</p>
       <Taskform addTask = {addTask}/>
-      <Tasklist />
+      <TaskList tasks = {tasks} 
+      updateTask = {updateTask}
+      deleteTask = {deleteTask}/>
       <Progresstracker />
       <button>Clear all tasks</button>
 
     </div>
   )
 }
- 
+
